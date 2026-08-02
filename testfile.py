939e361cff03307b176a3e807b27e4faa9a3890e
Dag0-5 <gulliverdavid05@gmail.com>
2026-08-02 18:40:58 +0100

    Test versions of basic webscraping
    Basic methods for exporting and iterating through URLs over a list

---
+++ testfile.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+def page_scraper(url,wordlist):
+    collection = list()
+    for word in wordlist:
+        try:
+            page = requests.get(url+word+"/")
+            soup = BeautifulSoup(page.text, "html.parser")
+            page.raise_for_status()
+
+            quotes = soup.find_all("span",attrs={"class":"text"})
+            authors = soup.find_all("small",attrs={"class":"author"})
+            collection.append((zip(quotes,authors),word))
+            print(word)
+        except ConnectionError:
+            print("ConnectionError occurred")
+            break
+        except TimeoutError:
+            print("TimeoutError occurred")
+            break
+        except requests.HTTPError:
+            print("HTTP error occurred")
+            break
+            
+        
+    return collection
+
+
+def export(file_name, collection):
+    file = open(file_name,"w", encoding="utf-8")
+    writer = csv.writer(file)
+    writer.writerow(["QUOTE","AUTHOR","WORD"])
+    for (references,word) in collection:
+        for quote,author in references:
+            print(quote.text+" - "+author.text+" - "+word)
+            writer.writerow([quote.text,author.text,word])
+    file.close()
+
+url = "https://quotes.toscrape.com/tag/"
+collection = page_scraper(url,["love","inspirational","life","humor","books","reading","not a tag","friendship","friends","truth","simile"])
+export("testdump.csv",collection)
